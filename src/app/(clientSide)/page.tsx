24174fad362086ad2fa30e467802b87466b01581
Rorisang Putu <rorisangputu@gmail.com>
2025-07-05 23:15:28 +0200

    Add WhyChooseUs component to HomePage layout

--- src/app/(clientSide)/page.tsx
+++ src/app/(clientSide)/page.tsx
@@ -1,11 +1,15 @@
+import FAQ from "@/components/FAQ";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
       <HowItWorks />
+      <WhyChooseUs />
+      <FAQ />
     </div>
   );
 }
