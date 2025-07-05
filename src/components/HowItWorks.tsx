7005f340431672703572beca133563ab9f072e97
Rorisang Putu <rorisangputu@gmail.com>
2025-07-05 23:15:59 +0200

    Refactor HowItWorks component for improved styling and layout consistency

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -6,12 +6,12 @@ import returnHome from "../../public/return.png";
 
 export default function HowItWorks() {
   return (
-    <section className="w-full px-6 py-20 bg-green-50">
+    <section className="w-full px-7 py-20 rounded-md">
       <div className="max-w-6xl mx-auto">
-        <h2 className="text-4xl font-extrabold text-green-700 text-center mb-4">
+        <h2 className="text-4xl font-extrabold  text-green-700 text-center mb-4">
           How Waggle Works
         </h2>
-        <p className="text-center text-lg text-gray-700 mb-12 max-w-2xl mx-auto">
+        <p className="text-center text-lg text-gray-700 mb-12  max-w-2xl mx-auto">
           We know life gets hectic — that&apos;s why we&apos;re here. Whether
           you&apos;re stuck in meetings or just overwhelmed, we&apos;ll walk
           your pup so you don&apos;t have to.
@@ -19,7 +19,7 @@ export default function HowItWorks() {
 
         <div className="space-y-10 sm:space-y-5">
           {/* Step 1 */}
-          <div className="flex flex-col md:flex-row items-center gap-8">
+          <div className="flex flex-col md:flex-row items-center gap-8 sm:py-20">
             <div className="w-full md:w-1/2">
               <Image
                 src={workingDog}
@@ -41,7 +41,7 @@ export default function HowItWorks() {
           </div>
 
           {/* Step 2 */}
-          <div className="flex flex-col md:flex-row-reverse items-center gap-8">
+          <div className="flex flex-col md:flex-row-reverse items-center gap-8 bg-green-50 px-10 py-10 md:py-12 rounded-xl">
             <div className="w-full md:w-1/2">
               <Image
                 src={walker}
@@ -63,7 +63,7 @@ export default function HowItWorks() {
           </div>
 
           {/* Step 3 */}
-          <div className="flex flex-col md:flex-row items-center gap-8">
+          <div className="flex flex-col md:flex-row items-center gap-8 py-20">
             <div className="w-full md:w-1/2">
               <Image
                 src={track}
@@ -85,7 +85,7 @@ export default function HowItWorks() {
           </div>
 
           {/* Step 4 */}
-          <div className="flex flex-col md:flex-row-reverse items-center gap-8">
+          <div className="flex flex-col md:flex-row-reverse items-center gap-8 bg-green-50 px-10 py-20 rounded-xl">
             <div className="w-full md:w-1/2">
               <Image
                 src={returnHome}
