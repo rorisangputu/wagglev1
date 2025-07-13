ffadbad80c0f9c218c4197f22d8fff2000a4390b
Rorisang Putu <rorisangputu@gmail.com>
2025-07-13 18:26:09 +0200

    removing nav and adding navbar component

--- src/app/(clientSide)/layout.tsx
+++ src/app/(clientSide)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/Footer";
-import { HeadLink, Nav, NavLink } from "@/components/Nav";
-import { AlignJustify } from "lucide-react";
+import NavBar from "@/components/NavBar";
 
 export const dynamic = "force-dynamic"; //Not caching admin page
 //force all admin page to be dynamic
@@ -12,22 +11,7 @@ export default function ClientLayout({
 }>) {
   return (
     <>
-      <Nav>
-        <div className="flex justify-end space-x-5">
-          <HeadLink href={"/sign-up"}>Contact Us</HeadLink>
-          <HeadLink href={"/sign-up"}>Login</HeadLink>
-        </div>
-        <div className="flex justify-between items-center ">
-          <h1 className="text-green-600 text-4xl font-bold">Waggle</h1>
-          <div className="hidden lg:flex space-x-2">
-            <NavLink href={"/"}>Home</NavLink>
-            <NavLink href={"/products"}>Products</NavLink>
-            <NavLink href={"/orders"}>My Orders</NavLink>
-          </div>
-
-          <AlignJustify className="text-black lg:hidden" />
-        </div>
-      </Nav>
+      <NavBar />
       <div className="w-[90%] xl:w-[70%] mx-auto my-6 min-h-screen">
         {children}
       </div>
