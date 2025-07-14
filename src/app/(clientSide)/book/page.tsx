1d450fab1cafaff53906321f9f2d99d7b6edea0a
Rorisang Putu <rorisangputu@gmail.com>
2025-07-14 21:44:59 +0200

    creating walk booking page

--- src/app/(clientSide)/book/page.tsx
+++ src/app/(clientSide)/book/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import walk from "../../../../public/Dog walking-rafiki.png";
 export default function BookingPage() {
-    
+  return (
+    <div>
+      <h1 className="text-gray-500 font-bold text-center">Book a walk</h1>
+      <div className="">
+        <Image src={walk} alt="image" />
+      </div>
+      <div>
+        <div className="flex justify-between items-end">
+          <h2 className="text-green-600 text-4xl font-bold">R 250.00</h2>
+          <p className="text-gray-400">p/walk</p>
+        </div>
+      </div>
+    </div>
+  );
 }
